933a76ed14e068282d2701230c3815824bb96148
Hassan <hassan.seedat07@gmail.com>
2022-08-29 19:59:55 +0100

    Create library

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // 1. Create a new library for us to interact with
+        ArrayList<Librarian> librarians = new ArrayList<Librarian>(0);
+        ArrayList<Member> members = new ArrayList<Member>(0);
+        ArrayList<BookItem> availableBooks = new ArrayList<BookItem>(0);
+        ArrayList<BookItem> rentedBooks = new ArrayList<BookItem>(0);
+
+        Library library = new Library( librarians, members, availableBooks,  rentedBooks);
+
+        // 2. Populate some available books in our library
+        String fileName = "C:\\Users\\User\\Desktop\\books_data.csv";
+
     }
 }
