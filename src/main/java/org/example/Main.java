9fe8ca41709a1aa650a962889254feff538255d6
Hassan <hassan.seedat07@gmail.com>
2022-08-29 20:16:19 +0100

    Create accounts for users and librarians

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,6 +20,20 @@ public class Main {
         library.addAvailableBook(bookItem2);
         library.addRentedBook(bookItem3);
 
+        Member member1 = new Member(123456,"Seth","39 Brooklyn Works", "hassan@test.com", new LibraryCard(true));
+        Member member2 = new Member(123455,"Dean","6a Sylvester Street", "Joe@test.com", new LibraryCard(true));
+        Member member3 = new Member(123454,"Roman","26 The Friary", "Moe@test.com", new LibraryCard(false));
 
+        library.addMember(member1);
+        library.addMember(member2);
+        library.addMember(member3);
+
+        Librarian librarian1 = new Librarian(636463,"Charlotte","", "Charlotte@thelibrary.com");
+        Librarian librarian2 = new Librarian(857323,"Sasha","", "Sasha@ggnoremail.com");
+        Librarian librarian3 = new Librarian(902578,"Bianca","", "Bianca@ggnoremail.com");
+
+        library.addLibrarian(librarian1);
+        library.addLibrarian(librarian2);
+        library.addLibrarian(librarian3);
     }
 }
